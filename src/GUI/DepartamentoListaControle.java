813427f99250d9e7f67bfc21d97251462a3fecff
V7ctor <joaovictor89437@gmail.com>
2019-12-11 14:27:50 -0300

    Tela de Formulário e Modal

--- src/GUI/DepartamentoListaControle.java
+++ src/GUI/DepartamentoListaControle.java
@@ -1,20 +1,29 @@
 package GUI;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import Aplicacao.Main;
+import GUI.util.Alertas;
+import GUI.util.Utils;
 import Modelo.Entidades.Departamento;
 import Modelo.Servicos.ServicoDepartamento;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class DepartamentoListaControle implements Initializable {
@@ -36,8 +45,9 @@ public class DepartamentoListaControle implements Initializable {
 	private ObservableList<Departamento> obsLista;
 
 	@FXML
-	public void onBtNovoAction() {
-		System.out.println("Botão Novo Clickado!");
+	public void onBtNovoAction(ActionEvent evento) {
+		Stage stagePai = Utils.stageAtual(evento);
+		criarTelaFormulario("/GUI/DepartamentoCadastro.fxml", stagePai);
 	}
 	
 	public void setServico(ServicoDepartamento servico) {
@@ -65,6 +75,23 @@ public class DepartamentoListaControle implements Initializable {
 		List<Departamento> lista = servico.pesquisarTodos();
 		obsLista = FXCollections.observableArrayList(lista);
 		tabelaDepartamento.setItems(obsLista);
-		
+	}
+	
+	private void criarTelaFormulario(String caminho, Stage stageOriginario) {
+		try {
+			FXMLLoader carregar = new FXMLLoader(getClass().getResource(caminho));
+			Pane painel = carregar.load();
+			
+			Stage novaTela = new Stage();
+			novaTela.setTitle("Entre com os Dados do Departamento");
+			novaTela.setScene(new Scene(painel));
+			novaTela.setResizable(false);
+			novaTela.initOwner(stageOriginario);
+			novaTela.initModality(Modality.WINDOW_MODAL);
+			novaTela.showAndWait();
+
+		}catch (IOException e) {
+			Alertas.mostrarAlerta("IOException", "Erro ao Abrir Tela: ", e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
